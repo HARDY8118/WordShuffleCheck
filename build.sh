7196ea04dd345793842829853ca74ea788c52067
HARDY8118 <5825564-HARDY8118@users.noreply.gitlab.com>
2023-11-14 23:33:59 +0530

    Added action to switch to release branch

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ for d in $(go tool dist list | egrep 'android|linux')
 do
     go env -w GOOS=$(echo $d | cut -d "/" -f 1) GOARCH=$(echo $d | cut -d "/" -f 2)
     go build -o "./build/$d/word" main.go
-done;
+done
